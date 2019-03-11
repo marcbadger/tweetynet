b24082e141ca495592d682f5bf17e9145da5c65b
Marc Badger <marc.badger@gmail.com>
2019-03-11 13:28:11 -0400

    add train to cli/__init__

--- src/tweetynet/cli/__init__.py
+++ src/tweetynet/cli/__init__.py
@@ -2,6 +2,7 @@
 creating learning curves, etc."""
 
 from .make_data import make_data
+from .train import train
 from .learncurve import learncurve
 from .summary import summary
 from .predict import predict
